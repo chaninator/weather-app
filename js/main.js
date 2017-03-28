ca860c59672f03722ccf146d411bd97c08c015de
Andrew Chan <accsuperstar@gmail.com>
2017-03-28 13:58:58 -0500

    added 5 day

--- js/main.js
+++ js/main.js
@@ -5,28 +5,30 @@ console.log('main.js loaded');
 $(document).ready(function() {
   var apiUrl = 'http://api.openweathermap.org/data/2.5/weather?';
   var apiKey = '9f7fe3b0ceb0a7cf1e86812469152bc0';
+  var weekday = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+  var d = new Date();
 
   //using custom fonts instead of weather images
 
   var dict = {
-    '01d':'wi-day-sunny',
-    '02d':'wi-day-cloudy',
-    '03d':'wi-cloud',
-    '04d':'wi-cloudy',
-    '09d':'wi-showers',
-    '10d':'wi-day-rain-mix',
-    '11d':'wi-thunderstorm',
-    '13d':'wi-snow',
-    '50d':'wi-fog',
-    '01n':'wi-night-clear',
-    '02n':'wi-night-alt-cloudy',
-    '03n':'wi-night-alt-cloudy-high',
-    '04n':'wi-cloudy',
-    '09n':'wi-night-alt-sprinkle',
-    '10n':'wi-night-alt-showers',
-    '11n':'wi-night-alt-thunderstorm',
-    '13n':'wi-night-alt-snow',
-    '50n':'wi-night-fog'
+    '01d': 'wi-day-sunny',
+    '02d': 'wi-day-cloudy',
+    '03d': 'wi-cloud',
+    '04d': 'wi-cloudy',
+    '09d': 'wi-showers',
+    '10d': 'wi-day-rain-mix',
+    '11d': 'wi-thunderstorm',
+    '13d': 'wi-snow',
+    '50d': 'wi-fog',
+    '01n': 'wi-night-clear',
+    '02n': 'wi-night-alt-cloudy',
+    '03n': 'wi-night-alt-cloudy-high',
+    '04n': 'wi-cloudy',
+    '09n': 'wi-night-alt-sprinkle',
+    '10n': 'wi-night-alt-showers',
+    '11n': 'wi-night-alt-thunderstorm',
+    '13n': 'wi-night-alt-snow',
+    '50n': 'wi-night-fog'
   };
 
 
@@ -42,8 +44,8 @@ $(document).ready(function() {
     });
 
     getWeather.done(function(response) {
-      console.log(response);
-      var city = response.name;
+      console.log('response', response);
+      var city = (response.name).toUpperCase();
       var temperature = Math.round(response.main.temp * 9 / 5 - 459.67);
       var humidity = response.main.humidity;
       var description = response.weather[0].description;
@@ -52,29 +54,17 @@ $(document).ready(function() {
       // replaced picture with weather fonts
       // var weatherIcon = 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png';
 
-
       console.log(dict[response.weather[0].icon]);
-
-      // console.log(city, temperature, humidity);
-      // console.log(weatherIcon);
-      console.log(flag);
 
       // put API response on the page
       $('.results .results-city').text(city);
       $('.temperature-container .temperature').text(temperature + 'º');
-      $('.humidity-container .humidity').text(humidity + '%');
-      $('.description-container .description').text(description);
-      // $('h2').append('<img src = "' + weatherIcon + '">');
-      $('.wi').addClass(icon);
-      $('.city-error').css('display', 'none');
-      $('.flag-icon').addClass('flag-icon-' + flag);
-      $('.wi').addClass(icon);
-
+      $('#wi').attr('class', '');
+      $('#wi').addClass('wi');
+      $('#wi').addClass(icon);
 
 
     });
-
-
 
 
     getWeather.fail(function(error) {
@@ -82,6 +72,46 @@ $(document).ready(function() {
     });
 
   }
+
+  //5 day forecast
+
+  function forecast(location) {
+    $.ajax({
+      dataType: 'json',
+      url: 'http://api.openweathermap.org/data/2.5/forecast/daily',
+      data: {
+        q: location,
+        units: 'metric',
+        appid: apiKey,
+        cnt: '5'
+      }
+    }).done(function(response) {
+      console.log('response2', response);
+      $('.forecast').empty();
+      var dayCounter = d.getDay();
+      for (i = 0; i <= 4; i++) {
+        if (dayCounter >= weekday.length - 1) {
+          dayCounter = 0
+        } else {
+          dayCounter += 1
+        }
+
+        var arr = [];
+        var length = response.list.length;
+
+        arr[i] = ("<div class='row'><h4 class='col-md-4' id='days'>" + weekday[dayCounter] + "<h4/><h4 class='col-md-4' id='max'>" + Math.round(response.list[i].temp.max) + 'º' + "</h4><h3 class='col-md-4' id='wi'></h3></div>");
+
+        // min temp above
+        // <h5 id='min'>" + Math.round(response.list[i].temp.min) + 'º' + "</h5>
+
+        $('.forecast').append(arr.join(' '));
+
+        
+
+      }
+    });
+  }
+
 
   function setHandlers() {
     $('.getWeatherData').on('submit', function(e) {
@@ -94,10 +124,10 @@ $(document).ready(function() {
 
   // flow of our web app
   function main() {
-    getWeatherData();
+    getWeatherData('Austin');
     setHandlers();
+    forecast('Austin');
   }
-
   main();
 
 });
